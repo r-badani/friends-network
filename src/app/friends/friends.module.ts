7cdfbbddd82120fe2fa5b02f4c6b7085a35de5cc
Rushabh Badani <rbadani@cvent.com>
2021-02-13 12:57:50 -0500

    feat(friend): adding friends feature module and base component

---
+++ src/app/friends/friends.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FriendsComponent } from './friends.component';
+
+
+
+@NgModule({
+  declarations: [FriendsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FriendsModule { }

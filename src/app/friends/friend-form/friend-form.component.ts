05f65c0d44b529f1ae154860f82480aee7788fab
Rushabh Badani <rbadani@cvent.com>
2021-02-13 14:30:26 -0500

    feat(form): injecting base form via formArray

--- src/app/friends/friend-form/friend-form.component.ts
+++ src/app/friends/friend-form/friend-form.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-friend-form',
   templateUrl: './friend-form.component.html',
   styleUrls: ['./friend-form.component.scss']
 })
-export class FriendFormComponent implements OnInit {
+export class FriendFormComponent {
+  @Input()
+  public baseForm!: FormGroup;
 
-  constructor() { }
+  @Input()
+  public formIndex!: number;
 
-  ngOnInit(): void {
+  static addForm(): FormGroup {
+    return new FormGroup({
+      name: new FormControl(''),
+      age: new FormControl(''),
+      weight: new FormControl(''),
+    });
   }
+  constructor() {}
 
 }
--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray } from '@angular/forms';
+import { FriendFormComponent } from './friend-form/friend-form.component';
 
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.component.html',
-  styleUrls: ['./friends.component.scss']
+  styleUrls: ['./friends.component.scss'],
 })
 export class FriendsComponent implements OnInit {
+  public group: FormGroup = new FormGroup({});
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get userFormArray(): FormArray {
+    return this.group?.get('users') as FormArray;
   }
 
+  get userFormGroup(): FormGroup[] {
+    return this.userFormArray?.controls as FormGroup[];
+  }
+
+  constructor() {}
+  ngOnInit(): void {
+    this.initializeUserFormGroup();
+  }
+
+  initializeUserFormGroup() {
+    this.group = new FormGroup({
+      users: new FormArray([FriendFormComponent.addForm()]),
+    });
+  }
+
+  onSubmit() {}
 }

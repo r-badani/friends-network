7c5c00aa270e00d6ddaa184808c50b03749fe660
Rushabh Badani <rbadani@cvent.com>
2021-02-13 14:07:35 -0500

    feat(form): adding basic form input for name, age, weight and required mat modules

--- src/app/friends/friends.module.ts
+++ src/app/friends/friends.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FriendsComponent } from './friends.component';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { FriendFormComponent } from './friend-form/friend-form.component';
-
+import { FriendsComponent } from './friends.component';
 
 
 @NgModule({
   declarations: [FriendsComponent, FriendFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
   ],
   exports: [FriendsComponent],
 })

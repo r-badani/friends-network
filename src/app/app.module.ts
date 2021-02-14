92dd6049d502e385ad7716ba2b65d488110f2f33
Rushabh Badani <rjbadani@gmail.com>
2021-02-13 21:11:58 -0500

    feat(main-page): building landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FriendsModule } from './friends/friends.module';
 import { FriendsService } from './friends/services/friends.service';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -13,6 +16,9 @@ import { FriendsService } from './friends/services/friends.service';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
     FriendsModule
   ],
   providers: [

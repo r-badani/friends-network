d3bbaf3b4714c97a03e0aefaca7ffa77e13c1a69
Rushabh Badani <rjbadani@gmail.com>
2021-02-14 01:23:52 -0500

    feat(chart): adding hover cart to show more user information

--- src/app/network-chart/network-chart.component.ts
+++ src/app/network-chart/network-chart.component.ts
@@ -64,7 +64,17 @@ export class NetworkChartComponent implements OnInit {
     const lineGenerator = d3.line();
     const h = this._options.height;
     const w = this._options.width;
+
+    // positioning hover card
+    if (this._currentNode !== null) {
+      const r = this._currentNode?.r.baseVal.value || 0;
+      const xPos = this._currentNode?.cx.baseVal.value + r + 3;
+      const yPos = this._currentNode?.cy.baseVal.value + r + 3;
+      this._card.attr('transform', `translate(${xPos},${yPos})`);
+    }
     // this._node.attr('cx', (d: any) => d.x).attr('cy', (d: any) => d.y);
+
+    // positioning nodes
     this._node
       .attr('cx', function (d: any) {
         return (d.x = Math.max(40, Math.min(w - 40, d.x)));
@@ -73,6 +83,7 @@ export class NetworkChartComponent implements OnInit {
         return (d.y = Math.max(40, Math.min(h - 40, d.y)));
       });
 
+    // positioning the links
     this._link.attr('d', (d: any) => {
       return lineGenerator([
         [d.source.x, d.source.y],
@@ -80,6 +91,7 @@ export class NetworkChartComponent implements OnInit {
       ]);
     });
 
+    // positioning image inside node
     this._image.attr('transform', (d: any) => {
       return `translate(${d.x - 35}, ${d.y - 35})`;
     });
@@ -129,9 +141,25 @@ export class NetworkChartComponent implements OnInit {
       .attr('class', 'node')
       .attr('r', (d: any) => 40)
       .attr('stroke', '#fbae17')
-      .attr('stroke-width', 2)
-      .style('fill', (d: any) => this._colorScale(d.id));
+      .attr('stroke-width', 1)
+      .style('fill', (d: any) => this._colorScale(d.id))
+      .on('mouseover', this.onMouseOver)
+      .on('mouseout', this.onMouseOut);
   }
+
+  private onMouseOut = (event: any, d: any) => {
+    this._currentNode = null;
+    this._card.attr('display', 'none');
+  };
+  private onMouseOver = (event: any, d: any) => {
+    this._currentNode = event.target;
+    this._card.cardTextName.text(`${d.name}`);
+    this._card.cardTextAge.text(`Age: ${d.age} yrs`);
+    this._card.cardTextWeight.text(`Weight: ${d.weight} lbs`);
+
+    this._card.attr('display', 'block');
+    this._simulation.alphaTarget(0).restart();
+  };
 
   private insertImage() {
     this._image = this._svg
@@ -143,11 +171,9 @@ export class NetworkChartComponent implements OnInit {
       .append('image')
       .attr('height', (d: any) => 70)
       .attr('width', (d: any) => 70)
-      // .attr('height', (d: any) => nodeScale(d.weight))
-      // .attr('width', (d: any) => nodeScale(d.weight))
       .attr('href', (d: any) => {
         var imagePath = `https://avatars.dicebear.com/4.5/api/avataaars/${
-          d.name
+          d.name + d.age + d.weight
         }.svg?radius=50&mouth[]=smile&bold=1&fontSize=70&background=${encodeURIComponent(
           '#f7fde6'
         )}`;
@@ -156,10 +182,43 @@ export class NetworkChartComponent implements OnInit {
       .attr('pointer-events', 'none');
   }
 
+  private hoverCard() {
+    this._card = this._svg
+      .append('g')
+      .attr('pointer-events', 'none')
+      .attr('display', 'none');
+
+    this._card.cardBackground = this._card
+      .append('rect')
+      .attr('width', 175)
+      .attr('height', 50)
+      .attr('fill', 'white')
+      .attr('stroke', '#fbae17')
+      .attr('stroke-width', 2)
+      .attr('rx', 5);
+
+    this._card.cardTextName = this._card
+      .append('text')
+      .attr('font-size', 16)
+      .attr('transform', 'translate(8, 20)')
+      .text('');
+    this._card.cardTextWeight = this._card
+      .append('text')
+      .attr('font-size', 12)
+      .attr('transform', 'translate(8, 40)')
+      .text('');
+    this._card.cardTextAge = this._card
+      .append('text')
+      .attr('font-size', 12)
+      .attr('transform', 'translate(100, 40)')
+      .text('');
+  }
+
   private generateChart() {
     this.drawLinks();
     this.drawNodes();
     this.insertImage();
+    this.hoverCard();
   }
 
   constructor(private service: FriendsService) {}
